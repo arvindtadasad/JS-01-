35bec21958e9d638c6e41379ded02f3f8b5fc924
arvindtadasad <arvindtadasad77@gmail.com>
2024-08-27 20:47:00 +0530

    expenditure-analysis

--- easy/expenditure-analysis.js
+++ easy/expenditure-analysis.js
@@ -14,8 +14,42 @@
 */
 
 function calculateTotalSpentByCategory(transactions) {
-	console.log('tesying');
-	return [];
+	let categoryFound;
+	let output = []; // This will be the final output.
+
+	// "transactions" is a list of objects, each object having these components: { itemName, category, price, timestamp };
+	// I need to collect "category" & "price" of each object.
+	indexes1 = transactions.length;
+	// Iterating over "transactions" array using indexes:
+	for (let i = 0; i < indexes1; i++) {
+		// For each object:
+		// category -> transactions[i]["category"]
+		// price -> transactions[i]["price"]
+
+		// I need to see if this "category" already exists in "output";
+		// To check in "output", I need to iterate over it.
+		categoryFound = false;
+		indexes2 = output.length;
+		for (let j = 0; j < indexes2; j++) {
+			if (transactions[i]["category"] == output[j]["category"]) {
+				// "category" already exists in "output";
+				// I need to update the "price" to sum of existing & new.
+				output[j]["totalSpent"] = output[j]["totalSpent"] + transactions[i]["price"];
+				categoryFound = true;
+				break;
+			}
+		}
+
+		if (categoryFound == false) {
+			// "category" does NOT exist in "output";
+			// Push this new object for new category found.
+			output.push({
+				category: transactions[i]["category"],
+				totalSpent: transactions[i]["price"]
+			})
+		}
+	}
+	return output;
 }
 
 module.exports = calculateTotalSpentByCategory;
